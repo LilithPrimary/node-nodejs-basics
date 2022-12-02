ffffff89417f58334c8a34033d6b93ec684156c3
Denna <lilithprimary@gmail.com>
2022-12-02 23:02:27 +0100

    feat: list.js is done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { readdir } from 'fs/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const list = async () => {
-    // Write your code here 
+  const error = Error('FS operation failed');
+
+  const path = join(__dirname, 'files');
+
+  const files = await readdir(path).catch(() => {
+    throw error;
+  });
+  console.table(files);
 };
 
 await list();
