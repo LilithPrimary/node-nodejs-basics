21818bc7184058597a01ff8137928ae458560366
Denna <lilithprimary@gmail.com>
2022-12-02 15:17:27 +0100

    feat: task create.js is done

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import { appendFile, access } from 'fs/promises';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+  const err = Error('FS operation failed');
+  const dir = join(__dirname, 'files', 'fresh.txt');
+  access(dir)
+    .then(() => {
+      console.log('error');
+      throw err;
+    })
+    .catch((error) => {
+      if (error === err) {
+        throw err;
+      }
+      appendFile(dir, 'I am fresh and young');
+    });
 };
 
 await create();
