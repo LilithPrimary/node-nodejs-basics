1b529609802028888be71785b05d83a175f858e6
Denna <lilithprimary@gmail.com>
2022-12-04 23:29:51 +0100

    feat: done zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,17 @@
+import { createUnzip } from 'zlib';
+import { createWriteStream, createReadStream } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const decompress = async () => {
-    // Write your code here 
+  const input = createReadStream(join(__dirname, 'files', 'archive.gz'));
+  const output = createWriteStream(
+    join(__dirname, 'files', 'fileToCompress.txt')
+  );
+  const decomp = createUnzip();
+  input.pipe(decomp).pipe(output);
 };
 
 await decompress();
