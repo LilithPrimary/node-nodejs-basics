a61a6013085b8d1b96ecea8acec3ea4c13ea70f8
Denna <lilithprimary@gmail.com>
2022-12-04 20:34:12 +0100

    feat: streams read.js in done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = await readFile(
+    join(__dirname, 'files', 'fileToCalculateHashFor.txt'),
+    { encoding: 'utf8' }
+  );
+
+  const hash = createHash('sha256');
+
+  const output = hash.update(input).digest('hex');
+
+  console.log(output);
 };
 
 await calculateHash();
